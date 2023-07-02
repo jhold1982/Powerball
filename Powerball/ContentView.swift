17ed94350c1a28fa5d775f6cfbcbeab08f042b57
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2023-07-01 21:14:39 -0500

    First Commit

--- Powerball/ContentView.swift
+++ Powerball/ContentView.swift
@@ -8,19 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+	
+	// MARK: - PROPERTIES
+	@State private var randomNumbers: [Int] = []
+
+	
+	
+	// MARK: - BODY
+	var body: some View {
+		NavigationStack {
+			ZStack {
+				Color.orange
+					.ignoresSafeArea()
+				VStack {
+					Text("Your lucky numbers")
+						.padding()
+					HStack {
+						ForEach(randomNumbers, id: \.self) { number in
+							Text("\(number)")
+								.font(.system(size: 20))
+								.padding()
+						}
+					}
+					.padding(.top, 10)
+					Spacer()
+					Button {
+						generateRandomNumbers()
+					} label: {
+						Image(systemName: "dollarsign")
+							.resizable()
+							.scaledToFit()
+							.foregroundColor(.green)
+							.frame(width: 200, height: 200)
+					} //: END OF BUTTON
+				} //: END OF VSTACK
+				.navigationTitle("Powerball")
+			} //: END OF ZSTACK
+		}
+	}
+	// MARK: - FUNCTIONS
+	/// This function creates an array of 6 random numbers in the range of 1 to 70
+	func generateRandomNumbers() {
+		randomNumbers = (1...6).map { _ in
+			Int.random(in: 1...70)
+		}
+	}
 }
 
+// MARK: - PREVIEWS
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+	static var previews: some View {
+		ContentView()
+	}
 }
