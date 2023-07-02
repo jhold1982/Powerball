4b6621284c6d54aa6f203681cf39c35583143abe
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2023-07-01 21:12:48 -0500

    Initial Commit

---
+++ Powerball/PowerballApp.swift
@@ -0,0 +1,17 @@
+//
+//  PowerballApp.swift
+//  Powerball
+//
+//  Created by Justin Hold on 7/1/23.
+//
+
+import SwiftUI
+
+@main
+struct PowerballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
